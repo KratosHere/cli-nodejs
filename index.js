f03ab5022ef72339faff86bcc14d5b48f58db88f
shubhinux <KratosHere>
2024-11-28 17:28:12 +0530

    some commands added

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ program
   .version('0.8.0');
 
 
-program.command("shubh")
-    .description("A CLI made by Shubh to perform File Based Operations!")
-    .action(() => {
-        console.log("WELCOME, And work more on this!")
+// program.command("shubh")
+//     .description("A CLI made by Shubh to perform File Based Operations!")
+//     .action(() => {
+//         console.log("WELCOME, And work more on this!")
+//     })
+
+program.command("read")
+    .argument("FILE_PATH")
+    .action((file_path) => {
+        fs.readFile(file_path , 'utf-8' , (err , data) => {
+            if (err) {
+                console.log(`Error Occured: ${err}`)
+            }
+            else {
+                console.log(data);
+            }
+        })
     })
+
 
 program.parse();
